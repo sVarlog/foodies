13ae9a519dbd94d677826248c768d47cbaf3ba6f
sVarlog <giroild@gmail.com>
2025-04-15 23:03:03 +0200

    feat: updated meal slug page

--- api/meals.ts
+++ api/meals.ts
@@ -6,9 +6,21 @@ const db = sql('meals.db');
 const getMeals = async (): Promise<Meal[]> => {
 	return new Promise((resolve) => {
 		setTimeout(() => {
-			resolve(db.prepare('SELECT * FROM meals').all() as Meal[]);
+			const meals = db.prepare('SELECT * FROM meals').all() as Meal[];
+
+			resolve(meals);
 		}, 200);
 	});
 };
 
-export { getMeals };
+const getMealBySlug = async (slug: string): Promise<Meal | undefined> => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const meal = db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug) as Meal;
+
+			resolve(meal);
+		}, 200);
+	});
+};
+
+export { getMeals, getMealBySlug };
--- app/meals/[slug]/page.tsx
+++ app/meals/[slug]/page.tsx
@@ -1,5 +1,68 @@
-const MealPage = () => {
-	return <h1>Meal details</h1>;
+import { getMealBySlug } from '@/api/meals';
+import Image from 'next/image';
+
+interface MealPageProps {
+	params: {
+		slug: string;
+	};
+}
+
+const MealPage = async ({ params }: MealPageProps) => {
+	const meal = await getMealBySlug(params.slug);
+
+	if (!meal) {
+		return <p>Meal not found</p>;
+	}
+
+	const getTrimedInstructions = (instructions: string) => {
+		const trimmedInstructions = instructions.split('\n');
+
+		// Remove empty strings and trim each instruction
+		const filteredInstructions = trimmedInstructions
+			.map((instruction) => instruction.trim())
+			.filter((instruction) => instruction !== '');
+		// Join the instructions with line breaks
+		const formattedInstructions = filteredInstructions.join('\n');
+
+		return (
+			<>
+				{formattedInstructions.split('\n').map((instruction, index) => (
+					<span key={index} className="my-[2rem] block text-2xl">
+						{instruction}
+					</span>
+				))}
+			</>
+		);
+	};
+
+	return (
+		<>
+			<header className="m-auto flex max-w-[80rem] gap-[3rem] px-[1rem] py-[2rem]">
+				<div className="relative h-[20rem] w-[30rem]">
+					<Image className="rounded-[8px] object-cover shadow-md" src={meal.image} fill alt={meal.title} />
+				</div>
+
+				<div className="max-w-[40rem] px-[1rem] pt-[0.5rem] text-[#ddd6cb]">
+					<h1 className="m-0 text-5xl font-bold">{meal.title}</h1>
+
+					<p className="py-[2rem] text-2xl text-[#cfa69b]">
+						by{' '}
+						<a href={`mailto:${meal.creator_email}`} className="font-bold text-yellow-500">
+							{meal.creator}
+						</a>
+					</p>
+
+					<p className="text-2xl">{meal.summary}</p>
+				</div>
+			</header>
+
+			<main>
+				<p className="mx-auto my-[2rem] rounded-[8px] bg-[#6e6464] p-[2rem] text-start text-2xl text-[#13120f] shadow-md">
+					{getTrimedInstructions(meal.instructions)}
+				</p>
+			</main>
+		</>
+	);
 };
 
 export default MealPage;
